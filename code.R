42e70b78e746c1183e15552a63f62043bdb50620
Guandi Xu <guandi@Guandis-MacBook-Pro.local>
2020-09-11 05:01:43 -0400

    using code

---
+++ code.R
@@ -0,0 +1,12 @@
+library(devtools)
+library(bookdown)
+
+#download the package
+
+devtools::install_github("hadley//sloop")
+devtools::install_github("hadley//emo")
+
+system(git clone https://github.com/hadley/adv-r.git)
+setwd("adv_r")
+
+bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
